b8dd15d1ac992f60a786b185b07937cf431be08f
msabhii <msabhithakur7777@gmail.com>
2025-04-22 18:49:58 +0530

    on hold

--- apps/X/app/api/user/route.ts
+++ apps/X/app/api/user/route.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { authOptions } from "app/lib/auth";
 import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
@@ -10,7 +10,6 @@ export const GET = async () => {
   if (!session) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
-
   const userID = session.user.id;
 
   try {
@@ -32,5 +31,48 @@ export const GET = async () => {
       status: 400,
       message: "Error while fetching from DB",
     });
+  }
+};
+
+interface EditUserInfoProp {
+  username: string;
+  bio: string;
+  location: string;
+  biolink: string;
+  dob: string;
+}
+
+export const POST = async (req: NextRequest, user: EditUserInfoProp) => {
+  console.log("Hitting here");
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    const body = await req.json();
+    console.log("Request Body:", body);
+    const userID = session?.user?.id;
+
+    const updatedUser = await prisma.user.update({
+      data: {
+        username: user.username,
+        bio: user.bio,
+        location: user.location,
+        bioLink: user.biolink,
+        DOB: user.dob,
+      },
+      where: { id: userID },
+    });
+
+    console.log(updatedUser, "<<<<<<<<<<<<<<<<<<<<<<<");
+    return NextResponse.json(
+      { message: "Profile updated successfully", data: updatedUser },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Getting error while Updating User Profile" },
+      { status: 500 }
+    );
   }
 };
